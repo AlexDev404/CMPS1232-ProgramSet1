01de8eb575efa031fdfed7d44444bbe9304c99a1
Nickolas-Skai <89269942+Nickolas-Skai@users.noreply.github.com>
2023-04-24 12:40:10 -0600

    Update mysort.hpp

--- mysort.hpp
+++ mysort.hpp
@@ -1,13 +1,14 @@
 class MySort
 {
 public:
-  Mysort();
-  int const SIZE=9;
+ 
+  const static int SIZE=9;
   int bucket[10][SIZE+1];
   void sort(int a[]);
 
   
 private:
+MySort Mysort();
   int determineMaximumDigits(int a[], int size);
   void distributeValues(int a[], int maximumDigits);
   void collectValues(int a[]);
